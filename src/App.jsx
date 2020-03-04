8327cb0e857baa4c19443bff43e9dc808fd3e6b8
Basruhe <sb.ruhe@gmail.com>
2020-03-04 19:51:45 +0100

    trying to get bootstrap glyph to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
+import "./App.css";
+import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ""
+    };
+  }
+
+  search() {
+    console.log("this.state looks like this: ", this.state);
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         {/* title */}
         <div className="App-title">Music master from App</div>
         {/* search function */}
-        <div>
-          <input placeholder="Search here....." />
-          <button>Submit</button>
-        </div>
+        {/* note; can this whole thing be replaced by a normal form?*/}
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an artist"
+              value={this.state.query}
+              onChange={event => {
+                this.setState({ query: event.target.value });
+              }}
+              // search when 'enter' is pressed.
+              onKeyPress={event => {
+                if (event.key === "Enter") {
+                  this.search();
+                }
+              }}
+            />
+            {/* <InputGroup>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup> */}
+          </InputGroup>
+        </FormGroup>
+        <button onClick={() => this.search()}>Search</button>
         {/* profiles */}
         <div className="Profile">
           <div>Artist Picture</div>
@@ -24,3 +56,5 @@ class App extends Component {
 }
 
 export default App;
+
+// notes: glyphicon currently not working. See part 5 of course(2/4)
